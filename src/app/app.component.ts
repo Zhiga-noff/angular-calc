e48e204da0bafe6a445e704b4fa9c21b6cbcdfb5
Kirill Zhiganov <zhiganov_k.n@mail.ru>
2024-01-17 00:17:47 +0300

    Почти повторил калькулятор с реакта

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  public calculatorButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+','-','C','='];
 }
--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-calc',
@@ -6,10 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent {
+  @Input('calculatorButtons') calculatorButtons
+
   public result = 0
   public symbolFlag = ''
-  public actionFlag = ''
+  public actionFlag: string | boolean = ''
   public count = 0
   public styleResult = 'fieldResult'
+
+  public numbersButtons = []
+  public actionsButtons = []
+
+  ngOnInit() {
+    this.filterButtonNumbers(this.calculatorButtons)
+
+  }
+
+  filterButtonNumbers(buttons) {
+    buttons.forEach(button => {
+      if (!isNaN(button)) {
+        this.numbersButtons.push(button)
+        return
+      }
+    })
+  }
+
+  clickToResultButton() {
+    // this.getResult()
+    this.count = 0
+    this.symbolFlag = ''
+    this.actionResult(this.actionFlag)
+    this.styleResult = 'fieldResult active'
+  }
+
+  clickToResetButton() {
+    this.symbolFlag = ''
+    this.result = 0
+    this.count = 0
+    this.styleResult = 'fieldResult'
+  }
+
+  changeFieldResult(action) {
+    if (this.actionFlag) {
+      this.actionFlag = action
+      this.styleResult = 'fieldResult'
+      return this.result = Number(action)
+    }
+
+    const number = `${this.result}` + action
+
+    return this.result = Number(number)
+  }
+
+  // getResult() {
+  //   if (this.symbolFlag === '+') {
+  //     this.actionFlag = '+'
+  //     return this.result = this.count + this.result
+  //   }
+  //   if (this.symbolFlag === '-') {
+  //     this.actionFlag = '-'
+  //     return this.result = this.count - this.result
+  //   }
+  // }
+
+  actionResult(action) {
+    if (this.actionFlag) {
+      return
+    }
+    this.actionFlag = !this.actionFlag
+  }
 
 }
